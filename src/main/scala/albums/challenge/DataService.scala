1e7baf90e853f0b9bf8e2a98bb0704cca2d89fcc
iljaganulevics <ilja@skenariolabs.com>
2024-01-29 18:46:31 +0200

    reverting back the Cache

--- src/main/scala/albums/challenge/DataService.scala
+++ src/main/scala/albums/challenge/DataService.scala
@@ -2,6 +2,7 @@ package albums.challenge
 
 import albums.challenge.models.{Data, Entry}
 import org.apache.logging.log4j.{LogManager, Logger}
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.MediaType
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.stereotype.Service
@@ -16,7 +17,7 @@ class DataService {
   private val uri: String =
     "https://itunes.apple.com/us/rss/topalbums/limit=200/json"
 
-  //  @Cacheable(Array("entry"))
+  @Cacheable(Array("entry"))
   def fetch(): List[Entry] = {
     logger.info("Fetching data")
 
